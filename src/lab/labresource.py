4a79596d8fa9032ee5e38f10c06902ce6df1d31a
Tony Salomone <dadmobile@gmail.com>
2025-09-18 09:36:56 -0400

    Add a base resource class.

---
+++ src/lab/labresource.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+
+class BaseLabResource(ABC):
+    """
+    Base object for all other resources to inherit from.
+
+    This wraps the standard file-system structure and internal access functions.
+    Lab resources have an associated directory and a json file with metadata.
+    """
+
+    def __init__(self, id):
+        self.id = id
+
+    @abstractmethod
+    def _get_dir(self):
+        """Get file system directory where this resource is stored."""
+        pass
