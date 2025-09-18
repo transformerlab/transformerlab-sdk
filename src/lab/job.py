633ced2405e27c88d68c54d01f0bd0a628c5a631
Tony Salomone <dadmobile@gmail.com>
2025-09-17 22:40:12 -0400

    Set job status

--- src/lab/job.py
+++ src/lab/job.py
@@ -41,7 +41,7 @@ class Job:
         with open(job_file, "w", encoding="utf-8") as f:
             json.dump(json_data, f, ensure_ascii=False)
 
-    def _update_json_data_field(self, key, value):
+    def _update_json_data_field(self, key: str, value):
         json_data = self._get_json_data()
         json_data[key] = value
         self._set_json_data(json_data)
@@ -60,8 +60,7 @@ class Job:
 
         status: str representing the status of the job
         """
-        # TODO
-        pass
+        self._update_json_data_field("status", status)
 
     def get_status(self):
         """
