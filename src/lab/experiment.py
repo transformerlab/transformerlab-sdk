c9c526a1b7cbbde2020e24c13a7adc0a9ab0c045
Tony Salomone <dadmobile@gmail.com>
2025-09-18 16:57:03 -0400

    Add function to create a new job in Experiment.

--- src/lab/experiment.py
+++ src/lab/experiment.py
@@ -1,5 +1,8 @@
+import os
+
 from . import dirs
 from .labresource import BaseLabResource
+from .job import Job
 
 
 class Experiment(BaseLabResource):
@@ -13,3 +16,25 @@ class Experiment(BaseLabResource):
     def _get_dir(self):
         """Abstract method on BaseLabResource"""
         return dirs.experiment_dir_by_name(self.id)
+
+    def _get_jobs_dir(self):
+        return os.path.join(self._get_dir(), "jobs")
+
+    def create_new_job():
+        jobs_dir = self._get_jobs_dir()
+
+        # Scan the jobs directory for subdirectories with numberic names
+        # Find the largest number and increment to get the new job ID
+        largest_numeric_subdir = 0
+        if os.path.isdir(jobs_dir):
+            for entry in os.listdir(jobs_dir):
+                if entry.isdigit():
+                    full_path = os.path.join(jobs_dir, entry)
+                    if os.path.isdir(full_path):
+                        job_id = int(entry)
+                        if job_id > largest_numeric_subdir:
+                            largest_numeric_subdir = job_id
+
+        new_job_id = largest_numeric_subdir + 1
+        new_job = Job(self.id, new_job_id)
+        return new_job
