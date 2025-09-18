f77e7060628c3e857c335c927cba0e2b5d3ccb89
Tony Salomone <dadmobile@gmail.com>
2025-09-18 10:03:01 -0400

    Experiment inherits from BaseLabResource

--- src/lab/experiment.py
+++ src/lab/experiment.py
@@ -1,7 +1,15 @@
-class Experiment:
+from . import dirs
+from .labresource import BaseLabResource
+
+
+class Experiment(BaseLabResource):
     """
     Base object for managing all config associated with an experiment
     """
 
     def __init__(self, experiment_id):
         self.id = experiment_id
+
+    def _get_dir(self):
+        """Abstract method on BaseLabResource"""
+        return dirs.experiment_dir_by_name(self.id)
