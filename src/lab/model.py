9c61d2eec262a76a07c21d323bf35ff7dc142ff3
Tony Salomone <dadmobile@gmail.com>
2025-09-24 10:00:38 -0400

    Add model resource

---
+++ src/lab/model.py
@@ -0,0 +1,9 @@
+from .labresource import BaseLabResource
+
+
+class Model(BaseLabResource):
+    def import_model(self, model_name, model_path):
+        """
+        Given a model name and path, create a new model that can be used in the workspace.
+        """
+        pass
