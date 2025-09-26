fb753ac666cd711b8b93499a0c76a776c9189715
deep1401 <gandhi0869@gmail.com>
2025-09-26 09:42:44 -0600

    Add dataset service to move datasets to filesystem

--- src/lab/__init__.py
+++ src/lab/__init__.py
@@ -2,5 +2,6 @@ from .dirs import WORKSPACE_DIR, HOME_DIR
 from .job import Job
 from .experiment import Experiment
 from .model import Model
+from .dataset import Dataset
 
-__all__ = ["WORKSPACE_DIR", "HOME_DIR", Job, Experiment, Model]
+__all__ = ["WORKSPACE_DIR", "HOME_DIR", Job, Experiment, Model, Dataset]
