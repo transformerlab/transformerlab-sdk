d814d8eb42cde9a1b889a115ad3737178760cd56
deep1401 <gandhi0869@gmail.com>
2025-10-21 13:54:35 -0600

    Cache Jobs in jobs.json and get cached jobs from there

--- src/lab/job.py
+++ src/lab/job.py
@@ -3,6 +3,7 @@ from werkzeug.utils import secure_filename
 
 from . import dirs
 from .labresource import BaseLabResource
+from .dirs import get_workspace_dir
 
 
 class Job(BaseLabResource):
@@ -77,7 +78,21 @@ class Job(BaseLabResource):
 
         status: str representing the status of the job
         """
+        old_status = self.get_status()
         self._update_json_data_field("status", status)
+        
+        # Only trigger cache rebuild if job transitions from RUNNING to completed
+        # (since we don't cache RUNNING jobs, but we do cache completed jobs)
+        if old_status == "RUNNING" and status != "RUNNING":
+            try:
+                from .experiment import Experiment
+                experiment_id = self.get_experiment_id()
+                if experiment_id:
+                    exp = Experiment(experiment_id)
+                    exp._trigger_cache_rebuild(workspace_dir=get_workspace_dir())
+            except Exception:
+                # Don't fail if cache rebuild trigger fails
+                pass
 
     def get_status(self):
         """
@@ -292,3 +307,15 @@ class Job(BaseLabResource):
         Mark this job as deleted.
         """
         self.update_status("DELETED")
+        
+        # Trigger cache rebuild since deleted jobs are removed from cache
+        # This is non-blocking - just adds to pending queue
+        try:
+            from .experiment import Experiment
+            experiment_id = self.get_experiment_id()
+            if experiment_id:
+                exp = Experiment(experiment_id)
+                exp._trigger_cache_rebuild(workspace_dir=get_workspace_dir())
+        except Exception:
+            # Don't fail if cache rebuild trigger fails
+            pass
