26b94b62ff2f6ca3e720094c8c418a2a3db420a2
deep1401 <gandhi0869@gmail.com>
2025-11-04 13:03:52 -0700

    Set the s3 file system mount correctly

--- src/lab/dirs.py
+++ src/lab/dirs.py
@@ -50,9 +50,9 @@ def get_workspace_dir() -> str:
     org_id = _current_org_id.get()
 
     if org_id:
-        # # If the storage URI is set, use it for the org workspace
-        # if _current_tfl_storage_uri.get() is not None:
-        #     return _current_tfl_storage_uri.get()
+        # If the storage URI is set, use it for the org workspace
+        if _current_tfl_storage_uri.get() is not None:
+            return _current_tfl_storage_uri.get()
         path = storage.join(HOME_DIR, "orgs", org_id, "workspace")
         storage.makedirs(path, exist_ok=True)
         return path
